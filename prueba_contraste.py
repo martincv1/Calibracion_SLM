3d41406d5810a8875a5d2a555d6d02e828555322
Labo <LABOJM@ptmntb0184.inti.local>
2025-05-08 15:41:34 -0300

    cambios

--- prueba_contraste.py
+++ prueba_contraste.py
@@ -2,16 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 
-imagen = cv2.imread('fotos_rot/3004_I30_0_T22_r.png')
+imagen = cv2.imread('fotos_rot/3004_I230_0_T22_r.png')
 plt.imshow(imagen)
 plt.show()
 
 index_find = imagen.argmax()
 index = np.unravel_index(index_find, imagen.shape)
 print(imagen[index])
-for i in range(len(imagen[:,0,0])):
-    for j in range(len(imagen[0,:,0])):
-        imagen[i,j,0] = int(np.floor(imagen[i,j,0]*255/imagen[index]))
+imagen *= 255//37
 
-plt.imshow(imagen[:,:,0])
+plt.imshow(imagen)
 plt.show()
